095deb9b3983fdd4a5d8f9ee39651985be0040c5
Aung Kyaw Kyaw <aung.kk1404@gmail.com>
2025-11-09 02:20:40 +0000

    submit Assignment 2

--- ex1.py
+++ ex1.py
@@ -1,3 +1,11 @@
 def find(s, n):
 # write your implementation here
+    for i in range(len(s)):
+        for j in range(i+1, len(s)):
+            if s[i] + s[j] == n:
+                return [i, j]
     return None
+
+print(find([2, 7, 11, 15], 9))
+print(find([3, 2, 4], 6))
+print(find([3, 3], 6)) 
